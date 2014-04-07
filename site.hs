088b6b3a91f43faf7dc3122bb98c48469eea7d1a
Sebastian Wiesner <lunaryorn@gmail.com>
2014-04-07 14:13:51 +0200

    Build tag pages

--- site.hs
+++ site.hs
@@ -3,11 +3,12 @@
 import qualified Data.Map as Map
 
 import Data.Maybe (fromJust)
-import Data.Monoid (mappend)
+import Data.Monoid ((<>))
 import Data.Time.Clock (UTCTime)
 import Data.Time.Format (parseTime,formatTime)
 import Hakyll
 import System.Locale (iso8601DateFormat,defaultTimeLocale)
+import Text.Printf (printf)
 
 addDate :: Metadata -> Routes
 addDate metadata = customRoute addDateToIdentifier
@@ -25,62 +26,89 @@ postRoute = gsubRoute "posts/" (const "") `composeRoutes`
             metadataRoute addDate `composeRoutes`
             setExtension "html"
 
+postCtx :: Context String
+postCtx = dateField "date" "%B %e, %Y" <> defaultContext
+
+tagsCtx :: Tags -> Context String
+tagsCtx = tagsField "tags"
+
+tagPageCtx :: String -> [Item String] -> Context String
+tagPageCtx tag posts =
+  constField "title" (printf "Posts tagged “%s”" tag) <>
+  listField "posts" postCtx (return posts) <>
+  defaultContext
+
 main :: IO ()
 main = hakyll $ do
-    match "images/*" $ do
-        route   idRoute
-        compile copyFileCompiler
+  tags <- buildTags "posts/*" (fromCapture "tags/*.html")
 
-    match "css/*" $ do
-        route   idRoute
-        compile compressCssCompiler
+  tagsRules tags $ \tag pattern -> do
+    route idRoute
+    compile $ do
+      posts <- recentFirst =<< loadAll pattern
+      let context = tagPageCtx tag posts
+      makeItem ""
+        >>= loadAndApplyTemplate "templates/post-list.html" context
+        >>= loadAndApplyTemplate "templates/page.html" context
+        >>= loadAndApplyTemplate "templates/default.html" context
+        >>= relativizeUrls
 
-    match "pages/*" $ do
-        route   $ setExtension "html"
-        compile $ pandocCompiler
-          >>= loadAndApplyTemplate "templates/page.html" defaultContext
-          >>= loadAndApplyTemplate "templates/default.html" defaultContext
-          >>= relativizeUrls
+  create ["tags/index.html"] $ do
+    route idRoute
+    compile $ do
+      cloud <- renderTagCloud 100 130 tags
+      let context = constField "title" "Tags" <> defaultContext
+      makeItem cloud
+        >>= loadAndApplyTemplate "templates/page.html" context
+        >>= loadAndApplyTemplate "templates/default.html" context
+        >>= relativizeUrls
 
-    match "posts/*" $ do
-        route postRoute
-        compile $ pandocCompiler
-            >>= loadAndApplyTemplate "templates/post.html"    postCtx
-            >>= loadAndApplyTemplate "templates/default.html" postCtx
-            >>= relativizeUrls
+  match "images/*" $ do
+    route   idRoute
+    compile copyFileCompiler
 
-    create ["archive.html"] $ do
-        route idRoute
-        compile $ do
-            posts <- recentFirst =<< loadAll "posts/*"
-            let archiveCtx =
-                    listField "posts" postCtx (return posts) `mappend`
-                    constField "title" "Archives"            `mappend`
-                    defaultContext
-            makeItem ""
-                >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
-                >>= loadAndApplyTemplate "templates/default.html" archiveCtx
-                >>= relativizeUrls
+  match "css/*" $ do
+    route   idRoute
+    compile compressCssCompiler
 
-    match "index.html" $ do
-        route idRoute
-        compile $ do
-            posts <- recentFirst =<< loadAll "posts/*"
-            let indexCtx =
-                    listField "posts" postCtx (return posts) `mappend`
-                    constField "title" "Home"                `mappend`
-                    defaultContext
+  match "pages/*" $ do
+    route   $ setExtension "html"
+    compile $ pandocCompiler
+      >>= loadAndApplyTemplate "templates/page.html" defaultContext
+      >>= loadAndApplyTemplate "templates/default.html" defaultContext
+      >>= relativizeUrls
 
-            getResourceBody
-                >>= applyAsTemplate indexCtx
-                >>= loadAndApplyTemplate "templates/default.html" indexCtx
-                >>= relativizeUrls
+  match "posts/*" $ do
+    route postRoute
+    compile $ pandocCompiler
+      >>= loadAndApplyTemplate "templates/post.html"    (tagsCtx tags <> postCtx)
+      >>= loadAndApplyTemplate "templates/default.html" postCtx
+      >>= relativizeUrls
 
-    match "templates/*" $ compile templateCompiler
+  create ["archive.html"] $ do
+      route idRoute
+      compile $ do
+          posts <- recentFirst =<< loadAll "posts/*"
+          let archiveCtx =
+                  listField "posts" postCtx (return posts) <>
+                  constField "title" "Archives"            <>
+                  defaultContext
+          makeItem ""
+              >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
+              >>= loadAndApplyTemplate "templates/default.html" archiveCtx
+              >>= relativizeUrls
 
+  match "index.html" $ do
+      route idRoute
+      compile $ do
+          posts <- recentFirst =<< loadAll "posts/*"
+          let indexCtx =
+                  listField "posts" postCtx (return posts) <>
+                  constField "title" "Home"                <>
+                  defaultContext
+          getResourceBody
+              >>= applyAsTemplate indexCtx
+              >>= loadAndApplyTemplate "templates/default.html" indexCtx
+              >>= relativizeUrls
 
---------------------------------------------------------------------------------
-postCtx :: Context String
-postCtx =
-    dateField "date" "%B %e, %Y" `mappend`
-    defaultContext
+  match "templates/*" $ compile templateCompiler
